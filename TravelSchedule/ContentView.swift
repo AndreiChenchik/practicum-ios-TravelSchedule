6634791d2ace3e25ded98e3a67fabd69c8fd8d8d
Andrei Chenchik <andrei@chenchik.me>
2024-03-28 23:25:06 +0100

    Add network generation layer

--- TravelSchedule/ContentView.swift
+++ TravelSchedule/ContentView.swift
@@ -5,20 +5,53 @@
 //  Created by andrei.chenchik on 28.03.24.
 //
 
+import OpenAPIURLSession
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+  var body: some View {
+    VStack {
+      Image(systemName: "globe")
+        .imageScale(.large)
+        .foregroundStyle(.tint)
+      Text("Hello, world!")
     }
+    .padding()
+    .task {
+      do {
+        try stations()
+      } catch {
+        print(error)
+      }
+    }
+  }
+
+  func stations() throws {
+    let client = try Client(
+      serverURL: Servers.server1(),
+      transport: URLSessionTransport()
+    )
+
+    let service = NearestStationsService(
+      client: client,
+      apikey: "KEY"
+    )
+
+    Task {
+      do {
+        let stations = try await service.getNearestStations(
+          lat: 59.864177,
+          lng: 30.319163,
+          distance: 50
+        )
+        print(stations)
+      } catch {
+        print(error)
+      }
+    }
+  }
 }
 
 #Preview {
-    ContentView()
+  ContentView()
 }
