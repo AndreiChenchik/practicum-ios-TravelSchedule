89365fc5c430368540795d2817105710f4fd9c9c
Andrei Chenchik <andrei@chenchik.me>
2024-03-28 21:08:11 +0100

    Init project

---
+++ TravelSchedule/TravelScheduleApp.swift
@@ -0,0 +1,17 @@
+//
+//  TravelScheduleApp.swift
+//  TravelSchedule
+//
+//  Created by andrei.chenchik on 28.03.24.
+//
+
+import SwiftUI
+
+@main
+struct TravelScheduleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
